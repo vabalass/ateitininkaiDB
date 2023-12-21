895d7e94daf43f062802ea4615d8027958818cc5
Vabalas <vabalas.balys@gmail.com>
2023-12-22 00:25:10 +0200

    created view for people

--- ateitiesDB/Controllers/AteitisController.cs
+++ ateitiesDB/Controllers/AteitisController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ateitiesDB.Interfaces;
+using ateitiesDB.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AteitisManagement.Controllers
 {
     public class AteitisController : Controller
     {
+        private readonly IPeopleRepository _peopleRepository;
+
+        public AteitisController(IPeopleRepository peopleRepository)
+        {
+            _peopleRepository = peopleRepository;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Person> people = _peopleRepository.GetAllPeople().ToList();
+
+            return View(people);
         }
     }
 }
---
+++ ateitiesDB/Repositories/UnitsRepository.cs
@@ -0,0 +1,17 @@
+﻿using ateitiesDB.Models;
+
+namespace ateitiesDB.Repositories
+{
+    public class UnitsRepository
+    {
+        private readonly AteitininkaiDbContext _context;
+        public UnitsRepository(AteitininkaiDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Person> GetAllPeople()
+        {
+            return _context.People.ToList();
+        }
+    }
+}
