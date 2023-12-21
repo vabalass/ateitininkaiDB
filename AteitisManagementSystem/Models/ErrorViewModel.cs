1dc002a103797ca504a0f157aadad50a0052424e
Vabalas <vabalas.balys@gmail.com>
2023-12-21 15:11:36 +0200

    created MVC project

---
+++ AteitisManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AteitisManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
