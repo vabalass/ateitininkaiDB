d0ae7889f72dc106d28d0fa760de311c26ed39de
Vabalas <vabalas.balys@gmail.com>
2024-02-05 19:33:01 +0200

    removed test projects

--- AteitisManagement/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AteitisManagement.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
